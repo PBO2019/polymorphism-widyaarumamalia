911dd9ee418923996c941db58d492d92a90607e1
Widya Arum <widyaarumamalia95@gmail.com>
2019-01-24 10:17:17 +0700

    AKU PASI BISA

---
+++ src/KelilingBujurSangkar.java
@@ -0,0 +1,17 @@
+public class KelilingBujurSangkar {
+    public static void main (String [] args){
+
+        KelilingBujurSangkar kll= new KelilingBujurSangkar();
+        kll.keliling();
+
+    }
+
+
+    public void keliling(){
+        int sisi =4;
+        int panjang =6;
+        int hasil;
+        hasil = panjang*sisi;
+        System.out.print ("Hasil kelililing Bujur sangkar ="+ hasil);
+    }
+}
---
+++ src/kelilinglingkarann.java
@@ -0,0 +1,17 @@
+public class kelilinglingkarann {
+    public static void main(String[] args) {
+        kelilinglingkarann kll = new kelilinglingkarann();
+        kll.keliling();
+    }
+
+    public void keliling() {
+
+
+        double phi = 3.14;
+        double r = 5;
+        double hasil;
+
+        hasil = 2 * phi * r;
+        System.out.print("Hail dari kelilinglingkaran =" + hasil);
+    }
+}
